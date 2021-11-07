21c040a6f5543561493337c5962b8c43896efca7
Ewen CLÉMENT <ewenclement@gmail.com>
2021-11-07 17:04:15 +1100

    mise en forme de la page du calcul du seuil de rentabilite

--- js/seuilrentabilite.js
+++ js/seuilrentabilite.js
@@ -1,46 +1,126 @@
+let tabResult = [];
+let ca = -1;
+let cf = -1;
+let cv = -1;
+let pvht = -1;
+
 $('#calculer').on('click', function () {
 
-  // implémenter plus tard quelque chose qui fait qu'on peut pas recalculer tant qu'on n'a pas changé
-  // au moins une autre valeur
-  let ca = $('#ca').val();
-  let cf = $('#cf').val();
-  let cv = $('#cv').val();
-  let pvht = $('#pvht').val();
+  // on ne peut pas recalculer tant qu'on n'a pas changé au moins une des quatre valeurs d'entrée
+  if (ca != $('#ca').val() || cf != $('#cf').val() || cv != $('#cv').val() || pvht != $('#pvht').val()) {
+    ca = $('#ca').val();
+    cf = $('#cf').val();
+    cv = $('#cv').val();
+    pvht = $('#pvht').val();
 
-  if (ca && cf && cv && pvht) {
-    console.log(ca, cf, cv, pvht);
-    calculResultat(ca, cf, cv);
-    calculTauxMarge(ca, cv);
-    calculSeuilValeur(ca, cf, cv);
-    calculSeuilVolume(ca, cf, cv, pvht);
+    // tous les champs doivent être remplis pour pouvoir procéder au calcul
+    if (ca && cf && cv && pvht) {
+      console.log(ca, cf, cv, pvht);
+
+      // calcul des différents éléments
+      let resultat = calculResultat(ca, cf, cv);
+      let tauxMarge = calculTauxMarge(ca, cv);
+      let seuilValeur = calculSeuilValeur(ca, cf, cv);
+      let seuilVolume = calculSeuilVolume(ca, cf, cv, pvht);
+
+      // on affiche le résultat dans l'élement correspondant
+      $('#resultat').html(`${resultat}`);
+      $('#tauxmarge').html(`${tauxMarge}`);
+      $('#seuilvaleur').html(`${seuilValeur}`);
+      $('#seuilvolume').html(`${seuilVolume} <span class='unite'>unité(s) à vendre</span>`);
+
+      // petit effet de fade-in lors du premier calcul
+      $('.result-number').css({
+        "opacity": "1"
+      });
+
+      // on pousse les données d'entrée et de sortie dans un tableau
+      let objectResult = {
+        ca: parseFloat(ca),
+        cf: parseFloat(cf),
+        cv: parseFloat(cf),
+        pvht: parseFloat(pvht),
+        resultat: resultat,
+        taux_marge: tauxMarge,
+        seuil_valeur: seuilValeur,
+        seuil_volume: seuilVolume
+      }
+      console.log(objectResult);
+
+      $('#calculer').addClass("disabled");
+    } else {
+      alert('Veuillez remplir tous les champs pour pouvoir procéder au calcul.');
+    }
   } else {
-    alert('Veuillez remplir tous les champs pour procéder au calcul.');
+    alert('Veuillez changer au moins champ pour pouvoir procéder à un nouveau calcul.');
   }
 });
 
 
 function calculResultat(ca, cf, cv) {
   let resultat = ca - cf - cv;
-  $('#resultat').html(`${resultat}`);
+  return resultat;
 }
 
 function calculTauxMarge(ca, cv) {
   let taux = (ca - cv) / ca;
   taux = +taux.toFixed(2);
-  $('#tauxmarge').html(`${taux}`);
   return taux;
 }
 
 function calculSeuilValeur(ca, cf, cv) {
-  let seuilValeur =  cf / calculTauxMarge(ca, cv);
+  let seuilValeur = cf / calculTauxMarge(ca, cv);
   seuilValeur = +seuilValeur.toFixed(2);
-  $('#seuilvaleur').html(`${seuilValeur}`);
   return seuilValeur;
 }
 
 function calculSeuilVolume(ca, cf, cv, pvht) {
-  let seuilVolume =  calculSeuilValeur(ca, cf, cv) / pvht;
+  let seuilVolume = calculSeuilValeur(ca, cf, cv) / pvht;
   seuilVolume = Math.round(seuilVolume);
-  $('#seuilvolume').html(`${seuilVolume} unité(s) à vendre`);
+  return seuilVolume;
 }
 
+
+$(".entree").on('input', function () {
+  if (ca != $('#ca').val() || cf != $('#cf').val() || cv != $('#cv').val() || pvht != $('#pvht').val()) {
+    $('#sauvegarder').removeClass("disabled");
+    $('#calculer').removeClass("disabled");
+  } else {
+    $('#sauvegarder').addClass("disabled");
+    $('#calculer').addClass("disabled");
+  }
+});
+
+$('#sauvegarder').on('click', function () {
+  appelAjax(objectResult);
+});
+
+function appelAjax(objectResult) {
+  const objectResultJson = JSON.stringify(objectResult);
+  console.log(objectResultJson);
+
+  /*$.ajax({
+      url: 'php/seuilrentabilite.php', // script qui va faire la requête
+      data: {
+        code: objectResultJson // JSON que l'on passe au script PHP
+      },
+      type: 'POST',
+      dataType: 'text'
+    })
+    .done(function (text) {
+		  alert("Calcul sauvegardé ✔️");
+      console.log(text); //pour voir les erreurs
+    })
+    .fail(function (xhr, status, errorThrown) {
+      alert("Sorry, there was a problem!");
+      console.log("Error: " + errorThrown);
+      console.log("Status: " + status);
+      console.dir(xhr);
+    })
+    .always(function (xhr, status) {
+      // à retirer à la fin
+      console.log("The request is complete!");
+    });*/
+
+  $('#sauvegarder').addClass("disabled");
+}
---
+++ php/seuilrentabilite.php
@@ -0,0 +1,5 @@
+<?php
+
+// the magic start here :)
+
+?>
